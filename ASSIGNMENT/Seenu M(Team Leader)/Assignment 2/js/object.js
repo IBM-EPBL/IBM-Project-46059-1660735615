30ff1480130bd14024bfa554fc71356c8516cace
Seenu M <69617552+Seenu-M@users.noreply.github.com>
2022-09-17 20:53:09 +0530

    Add files via upload
    Seenu M Assignment files

---
+++ ASSIGNMENT/Seenu M(Team Leader)/Assignment 2/js/object.js
@@ -0,0 +1,10 @@
+var car={
+    Name: "Audi",
+    Model: "A6",
+    Engine: "v12",
+    Price: 300000
+}
+console.log(car.Name);
+console.log(car.Model);
+console.log(car.Engine);
+console.log(car.Price);
