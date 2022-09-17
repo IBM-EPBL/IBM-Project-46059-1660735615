8ab239e892c29ed74f851a7763b4231d31939682
Seenu M <69617552+Seenu-M@users.noreply.github.com>
2022-09-17 14:38:24 +0530

    Assignment of 1 and 2

---
+++ ASSIGNMENT/Assignment 2/python/fifth.py
@@ -0,0 +1,10 @@
+def print_sum(first,second):
+    result=first+second;
+    print(result)
+
+    
+
+print_sum(5,6)
+print_sum(8,9)
+
+
---
+++ ASSIGNMENT/Assignment 2/python/fourth.py
@@ -0,0 +1,5 @@
+students=["deepak","seenu","sathish","MK"]
+for student in students:
+    if student == "sathish":
+        break;
+    print(student)
